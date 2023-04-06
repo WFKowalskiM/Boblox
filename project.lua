cca0e56685c5fc087378c248bd27189e2c811fbf
WFKowalskiM <90135966+WFKowalskiM@users.noreply.github.com>
2023-04-06 21:27:28 +0200

    Update project.lua

--- project.lua
+++ project.lua
@@ -1,21 +1,21 @@
-print('GWA')
-
-if not game:IsLoaded() then
-    game.Loaded:Wait()
-end
-
-wait(5)
-
 local u = game:GetService("UserInputService")
+print("Hello there")
 local store = game:GetService("ReplicatedStorage")
+print("Hello there")
 local me = game:GetService("Players").LocalPlayer
+print("Hello there")
 local value1 = me.Stats.Skill1.cooldown
 local value2 = me.Stats.Skill2.cooldown
 local value3 = me.Stats.Skill3.cooldown
+print("Hello there")
 local loopy = false
 print("Hello there")
 
-syn.queue_on_teleport("loadstring(game:HttpGet('https://pastebin.com/raw/dCDZrDcK', true))()")
+syn.queue_on_teleport[[
+repeat wait() until game:IsLoaded()
+wait(5)
+print('Loading scripto')
+"loadstring(game:HttpGet('https://pastebin.com/raw/dCDZrDcK', true))()"]]
 
 function attack()
     store.Events.attack:FireServer("Slash")
